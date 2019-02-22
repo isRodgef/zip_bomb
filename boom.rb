e0547659ecd2a7df6d8c7723cd2b11e09aed6edb
rodgef <rodgef101@gmail.com>
2019-02-22 15:10:00 +0200

    create a invalid zip file

--- boom.rb
+++ boom.rb
@@ -8,4 +8,7 @@ compressed_data = Deflate.deflate(data)
 puts(data.length)
 puts(compressed_data.length)
 
-File::new( "new.zip","rw")
+File::new( "new.zip","w")
+File.open("new.zip", "w") { 
+	|file| file.write("your text") 
+}
