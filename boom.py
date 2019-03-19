53748078d85667821b5fb963fb3fd75c8f55b3d5
rodgef <rodgef101@gmail.com>
2019-03-19 09:44:04 +0200

    can output to files

--- boom.py
+++ boom.py
@@ -1,11 +1,13 @@
 import zlib
+import zipfile
 
 test = "0000" * 1000
 
 cmpstr = zlib.compress(test.encode('utf-8'))
-print(len(cmpstr), "vs " , len(test)) 
 
 to_cmp = test + str(cmpstr)
 cmpstr = zlib.compress(to_cmp.encode('utf-8'))
 
-print(len(cmpstr))
+zf = zipfile.ZipFile("test.zip" ,mode='w',compression=zipfile.ZIP_DEFLATED)
+
+zf.writestr('a.txt',test)
