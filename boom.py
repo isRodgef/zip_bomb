0e8e0e2766bb671f60fd7598531c7c96cc22f236
rodgef <rodgef101@gmail.com>
2019-03-19 11:12:33 +0200

    added 500k iterations to test pc with"

--- boom.py
+++ boom.py
@@ -1,6 +1,9 @@
 import zlib
 import zipfile
 
+
+
+
 test = "0000" * 1000
 
 cmpstr = zlib.compress(test.encode('utf-8'))
@@ -10,4 +13,6 @@ cmpstr = zlib.compress(to_cmp.encode('utf-8'))
 
 zf = zipfile.ZipFile("test.zip" ,mode='w',compression=zipfile.ZIP_DEFLATED)
 
-zf.writestr('a.txt',test)
+for  i in range(500000):
+	print(i)
+	zf.writestr('a'+ str(i) +'.txt',test)
